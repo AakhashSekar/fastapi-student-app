841ca8e6ccc580eb6995f40b85502dc26dbf2c48
AakhashSekar <aakha2000@gmail.com>
2025-07-27 23:03:33 +0530

    updated env file

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL='postgresql://postgres:Abcd%40123@localhost:5432/student'
+load_dotenv()
+
+# DATABASE_URL='postgresql://postgres:Abcd%40123@localhost:5432/student'
+DATABASE_URL=os.getenv("DATABASE_URL")
 
 Engine = create_engine(DATABASE_URL)
 
-Session_Local = sessionmaker(autocommit=False, autoFlush = False, bind = Engine)
+Session_Local = sessionmaker(autocommit=False, autoflush = False, bind = Engine)
 
 Base = declarative_base()
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from database import Base
 class Students(Base):
     __tablename__ = 'student_details'
     id = Column(Integer, primary_key = True, index= True, autoincrement= True)
-    Student_name = Column(String, index = True)
+    name = Column(String, index = True)
     department = Column(String)
     age = Column(Integer)
     email = Column(String)
